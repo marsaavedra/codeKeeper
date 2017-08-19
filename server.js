2d32e504ff0c9d8aa156b650b7e04d8ad2fb059f
Andrew Dicer <andrew.dicer@gmail.com>
2017-08-19 15:49:10 -0400

    fixed server/coder/snips js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./routes/html-routes.js")(app);
-require("./routes/coder-api-routes.js")(app);
-require("./routes/snips-api-routes.js")(app);
+// require("./routes/html-routes.js")(app);
+// require("./routes/coder-api-routes.js")(app);
+// require("./routes/snips-api-routes.js")(app);
 
 db.sequelize.sync({ /*force: true*/ }).then(function() {
   app.listen(PORT, function() {
