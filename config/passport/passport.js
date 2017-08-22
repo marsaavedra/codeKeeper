c9b78f82e346766922ccc2ea75941dc3012ff09d
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-08-22 11:54:58 -0400

    Add comments

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -1,16 +1,22 @@
+// Dependencies
 var bCrypt = require('bcrypt');
 
 module.exports = function(passport, user){
 	var User = user;
 
+	//Local strategy for username/password authentication
 	var LocalStrategy = require('passport-local').Strategy;
 
-	//serialize
+	// In order to support login sessions, Passport will serialize and deserialize 
+	// user instances to and from the session.
+	// Here, the user ID is serialized to the session. 
+	// When subsequent requests are received, this ID is used to find the user
+	// which will be restored to req.user
 	passport.serializeUser(function(user, done){
 		done(null, user.id);
 	});
 
-	//deserialize user
+	// deserialize user
 	passport.deserializeUser(function(id, done) {
    	 	User.findById(id).then(function(user) {
 	        if (user) { 
@@ -21,34 +27,42 @@ module.exports = function(passport, user){
 	    });
 	});
 
+	// sign up strategy
 	passport.use('signup', new LocalStrategy(
+		// The verify callback for local authentication accepts username and password arguments by default.
+		// Set what request fields our usernameField and passwordField are.
 		{
 			usernameField: 'email',
 			passwordField: 'password',
-			passReqToCallback: true
+			passReqToCallback: true // aloows us to pass the entire request to the callback "done"
 		},
 
 		function(req, email, password, done){
+			// The hashed password generating function
+			// Param password and return a hashed password
 			var generateHash = function(password){
 				return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
 			};
 
+			// Check if the emial already exist in the database
 			User.findOne({
 				where: {
 					email: email
 				}
 			}).then(function(user){
 				if(user){
-					return done(null, false, "That email is already taken."
-					);
+					// If email exist return a message
+					return done(null, false, "That email is already taken.");
 				} else {
+					// get a hashed password
 					var userPassword = generateHash(password);
+					// Create an object with user info
 					var data = {
 						email: email,
 						password: userPassword,
 						name: req.body.name,
 					};
-					
+					// Insert user in database
 					User.create(data).then(function(newUser, created){
 					 	if(!newUser){
 					 		return done(null, false, null);
@@ -61,8 +75,9 @@ module.exports = function(passport, user){
 		}
 	));
 
-	//Signin
+	//Signin strategy
 	passport.use('signin', new LocalStrategy(
+		// Set what request fields our usernameField and passwordField are.
 		{
 			usernameField: 'email',
 			passwordField: 'password',
@@ -72,10 +87,12 @@ module.exports = function(passport, user){
 		function(req, email, password, done){
 			var User = user;
 
+			// Function to compare password entered with the one in database
 			var isValidPassword = function(userpass, password){
 				return bCrypt.compareSync(password, userpass);
 			}
 
+			// Check if the emial exist in the database
 			User.findOne({
 				where: {
 					email: email
@@ -83,15 +100,19 @@ module.exports = function(passport, user){
 			}).then(function(user){
 
 				if(!user) {
+					// If user does not exist
 					return done(null, false, "Email does not exist.");
 				}
-
+				// Validate password
 				if(!isValidPassword(user.password, password)){
 					return done(null, false, "Incorrect password.");
 				}
 
+				// If the user exist and password is correst
+				// get user info
 				var userinfo = user.get();
 
+				// Return user info
 				return done(null, userinfo);
 			});
 		}
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,28 @@
 var path = require("path");
 module.exports = function(app){
+	// Get routes for home
 	app.get('/', function(req, res){
  		res.sendFile(path.join(__dirname, "../public/index.html"));
+ 	});
+
+	// Get routes for sign up
+ 	app.get('/signup', function(req, res){
+ 		res.sendFile(path.join(__dirname, "../public/signup.html"));
+ 	});
+
+ 	// Get routes for sign in
+	app.get('/signin', function(req, res){
+ 		res.sendFile(path.join(__dirname, "../public/signin.html"));
+ 	});
+
+	// Get routes for dash
+ 	app.get('/dash', function(req, res){
+ 		// Only autenticated user can access this page
+ 		// If user is not autenticated, redirect to login
+		if(req.isAuthenticated()){
+ 			res.sendFile(path.join(__dirname, "../public/dash.html"));
+ 		} else {
+ 			res.redirect('/');
+ 		}
  	});
 }
