67f62bf6709313242cc69694d2ce63b76f06a8a6
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-08-21 21:05:47 -0400

    Sign in

--- routes/coder-api-routes.js
+++ routes/coder-api-routes.js
@@ -12,6 +12,19 @@ module.exports = function(app, passport){
  		res.sendFile(path.join(__dirname, "../public/signin.html"));
  	});
 
+	app.post("/signin", function(req, res, next){
+		console.log('in');
+
+		passport.authenticate("signin", function(err, coder, msg){
+			var data = {
+				error: err,
+				coder: coder,
+				msg: msg
+			}
+			return res.json(data);
+		})(req, res, next);
+	});
+
 	app.post("/signup", function(req, res, next){
 		passport.authenticate("signup", function(err, coder, msg){
 			var data = {
