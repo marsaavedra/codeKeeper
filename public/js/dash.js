c1384e1cf85a7d9b0fc5d378c44d890d6206fc74
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-08-25 22:53:36 -0400

    Update api/snippet

---
+++ public/js/dash.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/dawn");
+    editor.getSession().setMode("ace/mode/text");
+    //chrome, crimson_editor, dawn, github, 
+
+    $('#add-new').on("click", function(){
+        $('#modal').modal({
+            keyboard: false
+        });
+    });
+
+    $('#category').on('change', function(){
+        editor.getSession().setMode("ace/mode/" + $(this).val());
+    });
+
+
+    $('#save').on('click', function(){
+        var data = {
+                title: $("#title").val().trim(),
+                description: $("#description").val().trim(),
+                language: $("#category").val().trim(),
+                snippet: editor.getSession().getValue(),
+                liked: 3,
+                status: $('#privacy').val()
+            };
+
+        $.post('/api/snippets', data, function(res){
+            $("#title").val('');
+            $("#description").val('');
+            $("#category").val('');
+            editor.getSession().setValue('');
+            $('#privacy').val('');
+
+            $('#modal').modal('toggle');
+        }); 
+    });
+});
