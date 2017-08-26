9783863517d3857e4caefb2040950c1055487245
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-08-26 15:16:02 -0400

    Add table bookmarks

--- models/user.js
+++ models/user.js
@@ -2,23 +2,34 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
 
     password: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [6, 20]
+      }
     },
 
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        len: [6, 20]
+      }
     }
   });
 
-
-    User.associate = function(models) {
+  User.associate = function(models) {
     User.hasMany(models.Snips, {
+      onDelete: "cascade"
+    });
+  };
+
+  User.associate = function(models) {
+    User.hasMany(models.Bookmarks, {
       onDelete: "cascade"
     });
   };
